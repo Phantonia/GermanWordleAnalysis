9f8dd087d4ffb2b3410c8c3bcc56953fc2e6ca63
Antonia <62192270+Phantonia@users.noreply.github.com>
2022-01-23 13:06:15 +0100

    Finished (messy) code and wrote README

--- Phantonia.GermanWordleAnalysis/Program.cs
+++ Phantonia.GermanWordleAnalysis/Program.cs
@@ -1,2 +1,90 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// this is not gonna be optimized, if that would even be possible
+
+using System.Diagnostics;
+
+const int NumberOfLetters = 7;
+const double VowelPrioritizer = 1.0D;
+
+const string DictionaryPath = @"..\..\..\..\german.txt";
+
+string[] words = File.ReadAllLines(DictionaryPath);
+
+Dictionary<char, int> frequencies = new();
+
+for (char c = 'a'; c <= 'z'; c++)
+{
+    frequencies[c] = 0;
+}
+
+foreach (string word in words)
+{
+    foreach (char c in word.ToLower())
+    {
+        if (char.IsLetter(c))
+        {
+            frequencies[c]++;
+        }
+    }
+}
+
+long totalLetterCount = 0;
+
+foreach (int frequency in frequencies.Values)
+{
+    totalLetterCount += frequency;
+}
+
+Dictionary<char, double> relativeFrequencies = new();
+
+for (char c = 'a'; c <= 'z'; c++)
+{
+    relativeFrequencies[c] = (double)frequencies[c] / totalLetterCount;
+}
+
+bool EachLetterIsUnique(string word)
+{
+    Debug.Assert(word == word.ToLower());
+    return word.All(c => char.IsLetter(c)) && word.Distinct().Count() == word.Length;
+}
+
+double GetRank(string word)
+{
+    Debug.Assert(word == word.ToLower());
+
+    double sum = 0.0D;
+
+    foreach (char c in word)
+    {
+        double multiplier = 1.0D;
+
+        if (c is 'a' or 'e' or 'i' or 'o' or 'u')
+        {
+            // just a quick guess: testing vowels is slightly more important
+            // than consonants
+            multiplier = VowelPrioritizer;
+        }
+
+        sum += relativeFrequencies[c] * multiplier;
+    }
+
+    return sum;
+}
+
+var query = words.Select(w => w.ToLower()).Where(w => w.Length == NumberOfLetters && EachLetterIsUnique(w)).OrderByDescending(w => GetRank(w));
+
+HashSet<char> usedCharacters = new();
+
+int count = 0;
+
+foreach (string word in query)
+{
+    if (usedCharacters.Intersect(word).Count() == 0) // the word contains a fresh set of characters
+    {
+        count++;
+
+        Console.WriteLine($"[{count}]: {word}");
+        usedCharacters.UnionWith(word);
+    }
+}
+
+{ }
---
+++ README.md
@@ -0,0 +1,17 @@
+You know the game Wordle which is basically *the* thing in the internet lately? The following words have been proposed to be ideal to start guessing with as they cover a lot of common letters:
+
+* SIREN (or RISEN, they have the same effect)
+* OCTAL
+* DUMPY
+
+According to how many matches you have with SIREN you might consider also gussing OCTAL and even DUMPY.
+
+I wanted to know these words for a German version of Wordle, and especially also different numbers of letters. Here are my results:
+
+Assume vowels are as important as consonants:
+
+4 letters | 5 letters | 6 letters | 7 letters
+RENS | ERNST | INSERT | ANREIST
+THAI | GHALI | LOCHAU | LOGBUCH
+KLUG | DUMPF | --- | ---
+ODBC | --- | --- | ---
